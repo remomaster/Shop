f04494202a42087e8bd91dd29608ef5ea82581d5
Remo <remo@gmail.com>
2020-03-16 11:13:42 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sali from Laravel
                 </div>
 
                 <div class="links">
