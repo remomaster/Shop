114e943076f6af308f304ceab6e86a1a0597729b
remo <remo.spichtig@santismail.ch>
2020-05-25 20:32:55 +0200

    add img to shop

--- resources/views/shop/all.blade.php
+++ resources/views/shop/all.blade.php
@@ -8,6 +8,7 @@
 @foreach($products as $product)
     <div class="d-flex m-2">
         <div class="h6">{{ $product->name }}</div>
+        <img src="{{ url('/img/P2601_03.jpg') }}" />
     </div>
 @endforeach
 </div>
